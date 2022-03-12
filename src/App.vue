cb46d365278987ae288ec9e6c25b541805cce879
Daniil <ZDaniil2000@mail.ru>
2022-03-12 08:20:09 +0300

    Add new vue components

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
     <div class="app">
-        <form>
-            <h4>Создание поста</h4>
-            <input 
-            v-bind:value="title"
-            @input="title = $event.target.value" 
-            class="input" 
-            type="text" 
-            placeholder="Название"
-            >
-            <input 
-            v-bind:value="body"
-            @input="body = $event.target.value" 
-            class="input" 
-            type="text" 
-            placeholder="Описание"
-            >
-            <button 
-            class="btn" 
-            @click="createPost"
-            >
-            Создать</button>
-        </form>
-        <div class="post" v-for:="post in posts">
-            <div><strong>Название:</strong> {{ post.title }}</div> 
-            <div><strong>Описание:</strong> {{ post.body }}</div> 
-        </div>
+        <post-form/>
+        <post-list/>
     </div>
 </template>
 
 <script>
+import PostForm from "./components/PostForm.vue";
+import PostList from '@/components/PostList.vue';
 export default{
+  components: { 
+      PostForm, PostList },
     data() {
         return {
             posts:[
@@ -45,7 +25,15 @@ export default{
     },
     methods: {
         createPost() {
-
+            
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
+            this.title = '',
+            this.body = ''
         },
         
     }
@@ -63,30 +51,7 @@ export default{
     padding: 20px;
 }
 
-.post {
-    padding: 15px;
-    border: 5px solid teal;
-    margin-top: 15px;
-}
 
-form {
-    display: flex;
-    flex-direction: column;
-}
 
-.input{
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
 
-.btn{
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
-}
 </style>
